bbc3cefa07afbb2e6f26a87ac9c371b47adc7884
Sohammhatre10 <106436641+Sohammhatre10@users.noreply.github.com>
2023-07-16 18:55:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ const axios = require('axios')
 require('dotenv').config()
 const app = express()
 const apiKey = process.env.API_KEY
-let y = Math.pow(10,18);
+const createCSVwriter = require('csv-writer').createObjectCsvWriter
 const {utils} = require('ethers');
-
 class Block{
-    constructor(timestamp,blockReward){
+    constructor(timeStamp,blockReward){
         this.timeStamp=timeStamp;
         this.blockReward=blockReward
     }
@@ -16,20 +15,46 @@ class Block{
 const fetchData = async() =>{
     try{
         const listofBlocks = [];
-        for(let blockNumber=2165403; blockNumber<2165410; blockNumber++){
+        for(let blockNumber="Any Number"; blockNumber<"Any Number + Data required"; blockNumber++){
         const apiUrl = `https://api.etherscan.io/api?module=block&action=getblockReward&blockno=${blockNumber}&apikey=${apiKey}`
         const response = await axios.get(apiUrl);
-        const rewardEther = (response.data.result.blockReward/y)
+        const rewardEther = (response.data.result.blockReward/Math.pow(10,18))
         const timeStamp = response.data.result.timeStamp
         const block=new Block(timeStamp,rewardEther)
         listofBlocks.push(block)
         }
-        console.log(listofBlocks)
+        exportToCsv(listofBlocks)
     }catch(error){
         console.error(error)
     }
 }
-fetchData()
-app.listen(3000,()=>{
-    console.log("Server is running")
-})
+const exportToCsv = (data) => {
+    console.log("Heyy")
+    const csvWriter = createCSVwriter({
+        path: 'block_data.csv',
+        header: [
+            {id : 'timeStamp', title : 'timeStamp'},
+            {id : 'blockReward', title: 'blockReward'}
+        ]
+    });
+
+    csvWriter
+        .writeRecords(data)
+        .then(() => {
+            console.log('CSV file created successfully!');
+
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+(async() => {
+    try{
+        await fetchData()
+        app.listen(3000,()=>{
+            console.log("Server is running");
+        })
+    }catch(error){
+        console.error(error)
+    }
+})()
